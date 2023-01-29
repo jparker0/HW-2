1162368e2f3d7a5b3e816f10378c83ea7c847a0c
jparker0 <122473640+jparker0@users.noreply.github.com>
2023-01-29 15:39:15 -0500

    Add files via upload

---
+++ HW2_Parker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "This script can count the number of sequences in a fasta file"
+greeting="hello"
+name="Jen!"
+echo $greeting $name
+mkdir learning_bash
+cd learning_bash
+mkdir bioinformatics
+cd '/Users/jen/Desktop/Bioinformatics'
+cp '/Users/jen/Desktop/transcriptome.fasta'
+cd '/Users/jen/Desktop/Bioinformatics'
+grep -c ">" /Users/jen/Desktop/transcriptome.fasta >> "fasta_count.txt"
+
